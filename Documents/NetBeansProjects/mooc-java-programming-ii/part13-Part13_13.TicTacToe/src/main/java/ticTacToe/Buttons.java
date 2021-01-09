907e21f82a021226745832bea6c29680a41492b1
YelamanT <74448749+YelamanT@users.noreply.github.com>
2021-01-09 20:37:26 +0600

    Update Buttons.java

--- Documents/NetBeansProjects/mooc-java-programming-ii/part13-Part13_13.TicTacToe/src/main/java/ticTacToe/Buttons.java
+++ Documents/NetBeansProjects/mooc-java-programming-ii/part13-Part13_13.TicTacToe/src/main/java/ticTacToe/Buttons.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ticTacToe;
 
 import javafx.scene.Parent;
@@ -11,10 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 
-/**
- *
- * @author Yelaman
- */
+
 public class Buttons {
     private Label label;
     private String hey="X";
